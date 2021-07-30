4613fe90a52afd28062431f5d86799cc4e4c073a
Nao Julius <naojulius.mg@gmail.com>
2021-07-30 23:52:22 +0300

    fixed picklistmodel

--- application/config/routes.php
+++ application/config/routes.php
@@ -66,66 +66,66 @@ $route['api/job/demo'] = 'fixtures/DatabaseFixture/getJobDemo';
 
 
 
-$route['api/job/new']  = 'Job/Job/new'; //POST
-$route['api/job/delete'] = 'Job/Job/delete'; //POST
-$route['api/job/get'] = 'Job/Job/getById'; //GET BY ID
-$route['api/job/user/get'] = 'Job/Job/getByOwner'; //GET USER jobs
-$route['api/job/delete'] = 'Job/Job/deleteById'; //DELETE BY ID
-$route['api/job/follow'] = 'Follower/Follower/followJob'; //POST
-$route['api/job/unfollow'] = 'Follower/Follower/unfollowJob'; //POST
-$route['api/job/all'] = "Job/job/jobTable";
-$route['api/job/number'] = "Job/job/JobByLimit";
+$route['api/job/new']  = 'job/new'; //POST
+$route['api/job/delete'] = 'job/delete'; //POST
+$route['api/job/get'] = 'job/getById'; //GET BY ID
+$route['api/job/user/get'] = 'job/getByOwner'; //GET USER jobs
+$route['api/job/delete'] = 'job/deleteById'; //DELETE BY ID
+$route['api/job/follow'] = 'follower/followJob'; //POST
+$route['api/job/unfollow'] = 'follower/unfollowJob'; //POST
+$route['api/job/all'] = "job/jobTable";
+$route['api/job/number'] = "job/JobByLimit";
 /*
     picklists
 */
-$route['api/picklist/new'] = 'Picklist/Picklist/new';
-$route['api/picklist/get'] = 'Picklist/Picklist/getById';
-$route['api/picklist/delete'] = 'Picklist/Picklist/deleteById';
-$route['api/picklist/get/groupe'] = 'Picklist/Picklist/getByGroupe';
+$route['api/picklist/new'] = 'picklist/new';
+$route['api/picklist/get'] = 'picklist/getById';
+$route['api/picklist/delete'] = 'picklist/deleteById';
+$route['api/picklist/get/groupe'] = 'picklist/getByGroupe';
 /*
 annonces
 */
-$route['api/annonce/new']  = 'Annonce/Annonce/new'; //POST
-$route['api/annonce/delete'] = 'Annonce/Annonce/deleteById'; //GET
-$route['api/annonce/get'] = 'Annonce/Annonce/getById'; //GET BY ID
-$route['api/annonce/user/get'] = 'Annonce/Annonce/getByOwner'; //GET USER jobs
-$route['api/annonce/follow'] =  'Follower/Follower/followAnnonce'; //POST
-$route['api/annonce/unfollow'] = 'Follower/Follower/unfollowAnnonce'; //POST
-$route['api/annonce/all'] = "Annonce/Annonce/annonceTable";
-$route['api/annonce/number'] = "Annonce/Annonce/AnnonceByLimit";
+$route['api/annonce/new']  = 'annonce/new'; //POST
+$route['api/annonce/delete'] = 'annonce/deleteById'; //GET
+$route['api/annonce/get'] = 'annonce/getById'; //GET BY ID
+$route['api/annonce/user/get'] = 'annonce/getByOwner'; //GET USER jobs
+$route['api/annonce/follow'] =  'follower/followAnnonce'; //POST
+$route['api/annonce/unfollow'] = 'follower/unfollowAnnonce'; //POST
+$route['api/annonce/all'] = "annonce/annonceTable";
+$route['api/annonce/number'] = "annonce/AnnonceByLimit";
  /*
     rencontres
  */
-$route['api/rencontre/new']  = 'Rencontre/Rencontre/new'; //POST
-$route['api/rencontre/delete'] = 'Rencontre/Rencontre/deleteById'; //GET
-$route['api/rencontre/get'] = 'Rencontre/Rencontre/getById'; //GET BY ID
-$route['api/rencontre/user/get'] = 'Rencontre/Rencontre/getByOwner'; //GET USER jobs
-$route['api/rencontre/follow'] =  'Follower/Follower/followRencontre'; //POST
-$route['api/rencontre/unfollow'] = 'Follower/Follower/unfollowRencontre'; //POST
-$route['api/rencontre/all'] = "Rencontre/Rencontre/rencontreTable"; //POST
-$route['api/rencontre/number'] = "Rencontre/Rencontre/RencontreByLimit";
+$route['api/rencontre/new']  = 'rencontre/new'; //POST
+$route['api/rencontre/delete'] = 'rencontre/deleteById'; //GET
+$route['api/rencontre/get'] = 'rencontre/getById'; //GET BY ID
+$route['api/rencontre/user/get'] = 'rencontre/getByOwner'; //GET USER jobs
+$route['api/rencontre/follow'] =  'follower/followRencontre'; //POST
+$route['api/rencontre/unfollow'] = 'follower/unfollowRencontre'; //POST
+$route['api/rencontre/all'] = "rencontre/rencontreTable"; //POST
+$route['api/rencontre/number'] = "rencontre/RencontreByLimit";
 /*
     ventes
 */
-$route['api/vente/new']  = 'Vente/Vente/new'; //POST
-$route['api/vente/delete'] = 'Vente/Vente/deleteById'; //GET
-$route['api/vente/get'] = 'Vente/Vente/getById'; //GET BY ID
-$route['api/vente/user/get'] = 'Vente/Vente/getByOwner'; //GET USER jobs
-$route['api/vente/follow'] =  'Follower/Follower/followVente'; //POST
-$route['api/vente/unfollow'] = 'Follower/Follower/unfollowVente'; //POST
-$route['api/vente/all'] = "Vente/Vente/venteTable"; //POST
-$route['api/vente/number'] = "Vente/Vente/VenteByLimit";
+$route['api/vente/new']  = 'vente/new'; //POST
+$route['api/vente/delete'] = 'vente/deleteById'; //GET
+$route['api/vente/get'] = 'vente/getById'; //GET BY ID
+$route['api/vente/user/get'] = 'vente/getByOwner'; //GET USER jobs
+$route['api/vente/follow'] =  'follower/followVente'; //POST
+$route['api/vente/unfollow'] = 'follower/unfollowVente'; //POST
+$route['api/vente/all'] = "vente/venteTable"; //POST
+$route['api/vente/number'] = "vente/VenteByLimit";
 
 /*
    authentications 
 */
-$route['api/auth/login']  = 'User/Auth/login';
-$route['api/auth/registration']  = 'User/Auth/new';
-$route['api/user/get']  = 'User/Auth/getById';
+$route['api/auth/login']  = 'auth/login';
+$route['api/auth/registration']  = 'auth/new';
+$route['api/user/get']  = 'auth/getById';
 
 
 /*
    flash annonces
 */
 
-$route['api/flashannonce/number'] = "Annonce/FlashAnnonce/FlashAnnonceByLimit";
+$route['api/flashannonce/number'] = "flashAnnonce/FlashAnnonceByLimit";
