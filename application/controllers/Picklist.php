b70c3e6e12219000f1b45fd05ea90a43518b21f5
Nao Julius <naojulius.mg@gmail.com>
2021-07-31 07:22:31 +0300

    added headers to login

--- application/controllers/Picklist.php
+++ application/controllers/Picklist.php
@@ -82,22 +82,22 @@ class Picklist extends API_Controller
 		$data = $_GET["groupe"];
 		try {
 			if (!$data) {
-			  	$this->api_return(['status' => false,"data" =>"données insuffisante.",],400);exit;
+			  	$this->output
+			        ->set_content_type('application/json')
+			        ->set_output(json_encode(array('status' => true,"data" => "Donnée insuffisantes")));
 			}
 			$picklist = $this->PicklistModel->getByGroupe($data);
 			if(!$picklist){
-				$this->api_return(['status' => false,"data" =>"picklist introuvable.",],404);exit;
+				$this->output
+			        ->set_content_type('application/json')
+			        ->set_output(json_encode(array('status' => true,"data" => "Donnée introuvable")));
 			}
 			$this->output
 			        ->set_content_type('application/json')
 			        ->set_output(json_encode(array('status' => true,"data" => $picklist)));
 			
 		} catch (Exception $e) {
-		// $this->api_return(['status' => false,"data" =>"Erreur interne au serveur, veuillez contacter l //administrateur.",],400);exit;
-		$this->output
-			        ->set_content_type('application/json')
-			        ->set_output(json_encode(array('status' => true,"data" => "Erreur interne au serveur, veuillez contacter l'administrateur.")));
-
+		$this->api_return(['status' => false,"data" =>"Erreur interne au serveur, veuillez contacter l'administrateur.",],400);exit;
 		}
 	}
 }
